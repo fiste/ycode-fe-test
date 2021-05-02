38bac2c63ce1cd85254f1ce62d3976af708875b2
Stefan Mitrovic <fistee07@gmail.com>
2021-05-02 11:39:52 +0200

    Add fonts, modify default colors, add new icon component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,31 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      arial: ["Arial", "sans-serif"],
+      helvetica: ["Helvetica", "sans-serif"],
+      verdana: ["Verdana", "sans-serif"]
+    },
     extend: {
       colors: {
         gray: {
-          100: '#F8F8F8',
-          200: '#E7E7E7',
-          300: '#B3B6B7',
-          350: '#646768',
-          400: '#56595A',
-          450: '#3B3F40',
-          500: '#343839',
-          600: '#282c2d',
-          700: '#202425',
-          800: '#171B1C',
-          900: '#101314',
+          100: "#F8F8F8",
+          200: "#E7E7E7",
+          300: "#B3B6B7",
+          350: "#646768",
+          400: "#56595A",
+          450: "#3B3F40",
+          500: "#333436",
+          600: "#282c2d",
+          700: "#202425",
+          800: "#171B1C",
+          900: "#101314",
         },
-      }
+      },
     },
   },
   variants: {
-    extend: {
-
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
